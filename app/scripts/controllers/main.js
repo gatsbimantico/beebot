2cbba33d3930419dfa593728ccd79938b10687a7
gatsbimantico <gatsbimantico@gmail.com>
2016-04-09 16:52:17 +0200

    inicio de app

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,25 @@
 
 angular.module('gatsbimantico.Beebot')
 
-  .controller('MainCtrl', function($scope, $location, version) {
+.controller('MainCtrl', ['$scope', '$location', 'version', '$resource', function ($scope, $location, version, $resource) {
 
     $scope.$path = $location.path.bind($location);
     $scope.version = version;
 
-  });
+
+    var E = $resource("http://192.168.13.100:8080/sensor/2/historic");
+
+    window.console.log(E);
+    var entries = E.query(function () {
+        console.log(entries);
+    });
+
+    $scope.sensors = [
+        {
+            name: 'Sensor 1',
+            t: 25,
+            h: 0.8
+        }
+    ];
+
+}]);
